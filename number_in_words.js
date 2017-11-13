d0243e33526ac610fdc69ddea461212796668219
hendy <you@example.com>
2017-11-13 23:51:27 +0700

    number_in_words sudah realese 1

--- number_in_words.js
+++ number_in_words.js
@@ -8,19 +8,24 @@ function numberToWords(number) {
   else if (number < 200){ return 'seratus ' + numberToWords(number % 100) }
   else if (number < 1000){ return numberToWords(Math.floor(number / 100)) + ' ratus ' + numberToWords(Math.floor(number % 100))}
   else if( number < 2000){ return 'seribu ' + numberToWords(Math.floor(number % 1000))}
+  // else if( number < 2000){ return 'seribu ' + numberToWords(Math.floor(number % 1000))}
   else if( number < 10000){ return numberToWords(Math.floor(number / 1000)) + ' ribu ' + numberToWords(Math.floor(number % 1000))}
   else if( number < 20000){ return numberToWords(Math.floor(number / 1000))+ ' ribu ' + numberToWords(Math.floor(number % 1000))}
-  else if( number < 100000){ return numberToWords(Math.floor(number / 10000)) + ' ratus ribu ' + numberToWords(Math.floor(number % 10000))}
-  else if( number < 200000){ return 'seratus ribu ' + numberToWords(Math.floor(number % 100000))}
+  else if( number < 100000){ return numberToWords(Math.floor(number / 10000)) + ' puluh ' + numberToWords(Math.floor(number % 10000))}
+  else if( number < 200000){ return 'seratus ribu ' + numberToWords(number % 100000)}
+  else if( number < 1000000){ return  numberToWords(Math.floor(number % 100000))}
+  else if( number < 1000000000){ return  numberToWords(Math.floor(number / 1000000)) + 'juta ' + numberToWords(Math.floor(number % 1000000 ))}
+
+
 }
 // // Driver code
-console.log(numberToWords(19111));
+console.log(numberToWords(191111));
+// 90.191.111
 
 // module.exports = {
 //   numberToWords: numberToWords
-//}
-// console.log(20000 / 10000)
-
+// }
+console.log(191111 % 100000)
 // Driver code
 console.log(numberToWords(1));
 console.log(numberToWords(100));
