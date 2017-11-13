b3c054c54bafa0b4bde102eb980b4f222848c6ba
ariefmanda <arief.manda57@gmail.com>
2017-11-13 23:55:30 +0700

    Memperbaiki release 3

--- number_in_words.js
+++ number_in_words.js
@@ -1,68 +1,28 @@
 function numberToWords(number) {
   // Your code here
-  var satu=['nol','satu','dua','tiga','empat','lima','enam','tujuh','delapan','sembilan'];
-  var puluh=['nol','se','dua','tiga','empat','lima','enam','tujuh','delapan','sembilan'];
-  var satuan=['belas','puluh','ratus','ribu','puluh ribu','ratus ribu','juta','puluh juta','ratus juta','milyar','puluh milyar','ratus milyar','triliun','puluh triliun','ratus triliun'];
-  var hasil='';
-  var strNum=String(number);
-  if(strNum.length<=2){
-    if(number===0){
-      return ''
-    }else{
-      return hasil = satu[number];
-    }
-  }else if(number<20&&number>10){
-    return hasil = puluh[Math.floor(number/10)] +' '+ satuan[0]
-  }else {
-    if(number>10&&number<20){
-      hasil = puluh[Math.floor(number/10)] +' '+ satuan[1]
-    }
-    if(strNum.length===2){
-      hasil = puluh[Math.floor(number/10)] +' '+ satuan[1]
-      number=number%10;
-    }else if(strNum.length===3){
-      hasil = puluh[Math.floor(number/100)] +' '+ satuan[2]
-      number=number%100;
-    }else if(strNum.length===4){
-      hasil = puluh[Math.floor(number/1000)] +' '+ satuan[3]
-      number=number%1000;
-    }else if(strNum.length===5){
-      hasil = puluh[Math.floor(number/10000)] +' '+ satuan[4]
-      number=number%10000;
-    }else if(strNum.length===6){
-      hasil = puluh[Math.floor(number/100000)] +' '+ satuan[5]
-      number=number%100000;
-    }else if(strNum.length===7){
-      hasil = puluh[Math.floor(number/1000000)] +' '+ satuan[6]
-      number=number%1000000;
-    }else if(strNum.length===8){
-      hasil = puluh[Math.floor(number/10000000)] +' '+ satuan[7]
-      number=number%10000000;
-    }else if(strNum.length===9){
-      hasil = puluh[Math.floor(number/100000000)] +' '+ satuan[8]
-      number=number%100000000;
-    }else if(strNum.length===10){
-      hasil = puluh[Math.floor(number/1000000000)] +' '+ satuan[9]
-      number=number%1000000000;
-    }else if(strNum.length===11){
-      hasil = puluh[Math.floor(number/10000000000)] +' '+ satuan[10]
-      number=number%10000000000;
-    }else if(strNum.length===12){
-      hasil = puluh[Math.floor(number/100000000000)] +' '+ satuan[11]
-      number=number%100000000000;
-    }else if(strNum.length===13){
-      hasil = puluh[Math.floor(number/1000000000000)] +' '+ satuan[12]
-      number=number%1000000000000;
-    }else if(strNum.length===14){
-      hasil = puluh[Math.floor(number/10000000000000)] +' '+ satuan[13]
-      number=number%10000000000000;
-    }else if(strNum.length===15){
-      hasil = puluh[Math.floor(number/100000000000000)] +' '+ satuan[14]
-      number=number%100000000000000;
-    }
-    return hasil +' '+ numberToWords(number)
+  var satu=['','Satu','Dua','Tiga','Empat','Lima','Enam','Tujuh','Delapan','Sembilan','Sepuluh','Sebelas','Seratus ','Seribu '];
+  var satuan=[' Belas ',' Puluh ',' Ratus ',' Ribu ',' Juta ',' Milyar ',' Triliun ']
+  if(number<12){
+    return satu[number];
+  }else if(number>=12&&number<20){
+    return satu[number%10] + satuan[0]
+  }else if(number>=20&&number<100){
+    return numberToWords(Math.floor(number/10))+satuan[1]+ numberToWords(number%10)
+  }else if(number>=100&&number<200){
+    return satuan[12]+ numberToWords(number%100)
+  }else if(number>=200&&number<1000){
+    return numberToWords(Math.floor(number/100))+satuan[2]+ numberToWords(number%100)
+  }else if(number>=1000&&number<2000){
+    return satuan[13]+numberToWords(number%1000)
+  }else if(number>=2000&&number<1000000){
+    return numberToWords(Math.floor(number/1000))+satuan[3]+ numberToWords(number%1000)
+  }else if(number>=1000000&&number<1000000000){
+    return numberToWords(Math.floor(number/1000000))+satuan[4]+ numberToWords(number%1000000)
+  }else if(number>=1000000000&&number<1000000000000){
+    return numberToWords(Math.floor(number/1000000000))+satuan[5]+ numberToWords(number%1000000000)
+  }else if(number>=1000000000000&&number<1000000000000000){
+    return numberToWords(Math.floor(number/1000000000000))+satuan[6]+ numberToWords(number%1000000000000)
   }
-
 }
 
 // Driver code
