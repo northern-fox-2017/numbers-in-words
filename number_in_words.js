4df62a7fccf7c60256cf2f347872659621e9909c
Haniiful Wahib <wahib@Haniifuls-MacBook-Pro.local>
2017-11-13 23:29:19 +0700

    complete release 1 and release 2 by wahib

--- number_in_words.js
+++ number_in_words.js
@@ -1,9 +1,6 @@
 function numberToWords(number) {
   // Your code here
-
-  
   function belowHundredKata(num){
-
     function digitToKata(num){
       switch (num){
         case 1 : return 'satu'; break;
@@ -18,9 +15,7 @@ function numberToWords(number) {
         default : return '';
       }
     }
-
     function belasToKata(num){
-      
       if (num === 10){
         return 'sepuluh'
       }
@@ -28,12 +23,11 @@ function numberToWords(number) {
         return 'sebelas';
       }
       else if (num <= 19 && num >= 11){
-        return digitToKata(num%10) + 'belas';
+        return digitToKata(num%10) + ' belas';
       } else {
       return '';
       }
     }
-
     function puluhToKata(num){
         return digitToKata( Math.floor(num/10) ) + ' puluh ' + digitToKata(num%10);
     }
@@ -50,18 +44,28 @@ function numberToWords(number) {
 
   }
 
-  function ratusanToKata(num){
-    
-    if (num <= 99){
-      return belowHundredKata(num);
-    } else if (num <= 199 && num >= 100 ){
-      return 'seratus ' + belowHundredKata(num%100); 
-    } else if (num <= 999 && num >= 200){
-      return belowHundredKata( Math.floor(num/100) ) + ' ratus ' + belowHundredKata(num%100);
-    }
+  if (number <= 99 ) {
+    return belowHundredKata(number);
+  } else if (number >=100 && number <= 199) {
+    return 'seratus ' + belowHundredKata (number%100);
+  } else if (number >= 200 && number <= 999){
+    return belowHundredKata( Math.floor(number/100) ) + ' ratus ' + belowHundredKata(number%100);
+  } else if(number >= 1000 && number <= 1999){
+    return 'seribu ' + numberToWords(number%1000);
+  } else if (number >= 2000 && number <= 999999){
+    return numberToWords( Math.floor(number/1000) ) + ' ribu ' + numberToWords(number%1000);
+  } else if (number >= 1000000 && number <= 999999999){
+    return numberToWords( Math.floor(number/1000000) ) + ' juta ' + numberToWords(number%1000000);
+  } else if (number >= 1000000000 && number <= 999999999999){
+    return numberToWords( Math.floor(number/1000000000) ) + ' miliar ' + numberToWords(number%1000000000);
+  } else if (number >= 1000000000000 && number <= 999999999999999){
+    return numberToWords( Math.floor(number/1000000000000) ) + ' biliun ' + numberToWords(number%1000000000000);
+  } else if (number >= 1000000000000000 && number <= 999999999999999999){
+    return numberToWords( Math.floor(number/1000000000000000) ) + ' triliun ' + numberToWords(number%1000000000000000);
+  } else {
+    return '';
   }
-
-  return ratusanToKata(num);
+  
 }
 
 
