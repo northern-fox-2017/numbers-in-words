42ec75ad6608bf2dba8be8f56045f541c32b0af6
tobiaskusnaman@gmail.com <tobiaskusnaman@gmail.com>
2017-11-13 23:54:05 +0700

    DONE

--- number_in_words.js
+++ number_in_words.js
@@ -1,48 +1,67 @@
 function numberToWords(number) {
-  var res = [];
-  var library = [
-    [1, 'satu'],
-    [2, 'dua'],
-    [3, 'tiga'],
-    [4, 'empat'],
-    [5, 'lima'],
-    [6, 'enam'],
-    [7, 'tujuh'],
-    [8, 'delapan'],
-    [9, 'sembilan'],
-    [10, 'sepuluh'],
-    [11, 'sebelas'],
-    [12, 'dua belas'],
-    [100, 'seratus'],
+  var arr = [
+    '',
+    'satu',
+    'dua',
+    'tiga',
+    'empat',
+    'lima',
+    'enam',
+    'tujuh',
+    'delapan',
+    'sembilan',
+    'sepuluh',
+    'sebelas',
   ];
 
-  var separator = [' puluh ', ' ratus ', ' ribu '];
-  var split = number.toString().split('');
-  var panjang = number.toString().split('').length;
-
-  if (panjang === 1) {
-    return library[number - 1][1];
-  } else {
-    if (panjang === 2) {
-      return library[split[0] - 1][1] + separator[0] + numberToWords(split[1]);
-    }
-
-    if (panjang === 3) {
-      var angkaPuluhan = number % 100;
-      if (split[0] === 1) {
-        return 'seratus' + numberToWords(angkaPuluhan);
-      } else {
-        return library[split[0] - 1][1] + separator[1] + numberToWords(angkaPuluhan);
-      }
-    }
+  if (number < 12) {
+    return arr[number];
+  } else if (number < 20) {
+    return numberToWords(number % 10) + ' belas';
+  } else if (number < 100) {
+    return (
+      numberToWords((number - number % 10) / 10) +
+      ' puluh ' +
+      numberToWords(number % 10)
+    );
+  } else if (number < 200) {
+    return 'seratus ' + numberToWords(number % 100);
+  } else if (number < 1000) {
+    return numberToWords((number-(number%100))/100) + ' ratus ' + numberToWords(number%100);
+  } else if (number < 2000) {
+    return 'seribu ' + numberToWords(number % 1000);
+  } else if (number < 1000000) {
+    return (
+      numberToWords((number - number % 1000) / 1000) +
+      ' ribu ' +
+      numberToWords(number % 1000)
+    );
+  } else if (number < 1000000000) {
+    return (
+      numberToWords((number - number % 1000000) / 1000000) +
+      ' juta ' +
+      numberToWords(number % 1000000)
+    );
+  } else if (number < 1000000000000) {
+    return (
+      numberToWords((number - number % 1000000000) / 1000000000) +
+      ' miliar ' +
+      numberToWords(number % 1000000000)
+    );
+  } else if (number < 1000000000000000) {
+    return (
+      numberToWords((number - number % 1000000000000) / 1000000000000) +
+      ' triliun ' +
+      numberToWords(number % 1000000000000)
+    );
   }
 }
 
 // Driver code
-// console.log(numberToWords(1000000));
+console.log(numberToWords(1000000));
 console.log(numberToWords(4));
 console.log(numberToWords(999));
-console.log(numberToWords(304));
-console.log(numberToWords(422));
-// console.log(numberToWords(38079));
-// console.log(numberToWords(82102713));
+console.log(numberToWords(14));
+console.log(numberToWords(131));
+console.log(numberToWords(38079));
+console.log(numberToWords(82102713));
