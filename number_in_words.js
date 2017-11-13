838a3041be6530db51cd341825298518150b49f1
Luthfi <vlootfie@gmail.com>
2017-11-14 05:49:10 +0700

    release 2

--- number_in_words.js
+++ number_in_words.js
@@ -11,12 +11,29 @@ function numberToWords(number) {
     return 'seratus ' + numberToWords(number % 100);
   } else if (number < 1000) {
     return numberToWords((number - (number % 100)) / 100) + ' ratus ' + numberToWords(number % 100);
+  } else if (number < 2000) {
+    return 'seribu ' + numberToWords(number % 1000);
+  } else if (number < 1000000) {
+    return numberToWords((number - (number % 1000)) / 1000) + ' ribu ' + numberToWords(number % 1000);
+  } else if (number < 1000000000) {
+    return numberToWords((number - (number % 1000000)) / 1000000) + ' juta ' + numberToWords(number % 1000000);
+  } else if (number < 1000000000000) {
+    return numberToWords((number - (number % 1000000000)) / 1000000000) + ' milyar ' + numberToWords(number % 1000000000);
+  } else if (number < 1000000000000000) {
+    return numberToWords((number - (number % 1000000000000)) / 1000000000000) + ' trilyun ' + numberToWords(number % 1000000000000);
   }
 }
 
-console.log(numberToWords(999));
 // Driver code
-// console.log(numberToWords(1000000));
+console.log(numberToWords(4));
+console.log(numberToWords(27));
+console.log(numberToWords(102));
+console.log(numberToWords(10079));
+console.log(numberToWords(100079));
+console.log(numberToWords(1000011));
+console.log(numberToWords(820102712));
+console.log(numberToWords(90210002713));
+console.log(numberToWords(999000002000310));
 
 // module.exports = {
 //   numberToWords: numberToWords
