ab255cdc92ca4dd7a98e63bedfb920fb12986d45
Luthfi <vlootfie@gmail.com>
2017-11-13 22:50:03 +0700

    release 0

--- number_in_words.js
+++ number_in_words.js
@@ -1,10 +1,23 @@
 function numberToWords(number) {
-  // Your code here
+  var bilang = ['', 'satu', 'dua', 'tiga', 'empat', 'lima', 'enam', 'tujuh', 'delapan', 'sembilan', 'sepuluh', 'sebelas'];
+
+  if (number < 12) {
+    return bilang[number];
+  } else if (number < 20) {
+    return numberToWords(number % 10) + ' belas';
+  } else if (number < 100) {
+    return numberToWords((number - (number % 10)) / 10) + ' puluh ' + numberToWords(number % 10);
+  } else if (number < 200) {
+    return 'seratus ' + numberToWords(number % 100);
+  } else if (number < 1000) {
+    return numberToWords((number - (number % 100)) / 100) + ' ratus ' + numberToWords(number % 100);
+  }
 }
 
+console.log(numberToWords(999));
 // Driver code
-console.log(numberToWords(1000000));
+// console.log(numberToWords(1000000));
 
-module.exports = {
-  numberToWords: numberToWords
-}
+// module.exports = {
+//   numberToWords: numberToWords
+// }
