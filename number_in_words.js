35599a8e65b52e325506c150c1af89055ade59bd
Zaki Elyasa Djauhari <zakielyasadj@gmail.com>
2017-11-13 23:29:02 +0700

    done

--- number_in_words.js
+++ number_in_words.js
@@ -1,10 +1,89 @@
 function numberToWords(number) {
   // Your code here
-}
+  var huruf = ['', 'Satu', 'Dua', 'Tiga', 'Empat', 'Lima', 'Enam', 'Tujuh', 'Delapan', 'Sembilan']
+  // var angka = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  /*
+  if(number === 1){
+	  return angka[number]
+  } else if(number === 2){
+	  return angka[number]
+  } else if(number === 3){
+	  return angka[number]
+  } else if(number === 4){
+	  return angka[number]
+  } else if(number === 5){
+	  return angka[number]
+  } else if(number === 6){
+	  return angka[number]
+  } else if(number === 7){
+	  return angka[number]
+  } else if(number === 8){
+	  return angka[number]
+  } else if(number === 9){
+	  return angka[number]
+  } else if(number === 10){
+	  return 'Sepuluh'
+  } else if(number === 11){
+	  return 'Sebelas'
+  } else if(number < 20){
+	  return numberToWords(number - 10) + 'belas'
+  } else if(number < 100){
+	  return numberToWords(Math.floor(number/10)) + ' puluh ' + numberToWords(number%10)
+  } else if(number === 100){
+	  return 'Seratus'
+  } else if(number < 200){
+	  return 'seratus' + numberToWords(number % 100)
+  }
+  */
+    if(number == 0 ){
+      return huruf[number]
+    } else if(number == 1){
+        return huruf[number]
+    } else if(number == 2){
+        return huruf[number]
+    } else if(number == 3){
+        return huruf[number]
+    } else if(number == 4){
+        return huruf[number]
+    } else if(number == 5){
+        return huruf[number]
+    } else if(number == 6){
+        return huruf[number]
+    } else if(number == 7){
+        return huruf[number]
+    } else if(number == 8){
+        return huruf[number]
+    } else if(number == 9){
+        return huruf[number]
+    } else if(number == 10){
+        return 'sepuluh'
+    } else if(number == 11){
+        return 'sebelas'
+    } else if(number < 20){
+        return numberToWords(number - 10) + ' belas'
+    } else if(number < 100){
+        return numberToWords(Math.floor(number/10)) + ' puluh ' + numberToWords(number%10)
+    } else if(number < 200){
+        return 'seratus ' + numberToWords(number%100)
+    } else if(number < 1000){
+        return numberToWords(Math.floor(number/100)) + ' ratus ' + numberToWords(number%100)
+    } else if(number < 2000){
+		return 'seribu' + numberToWords(number%1000)
+	} else if(number < 1000000){
+		return numberToWords(Math.floor(number/1000)) + ' ribu ' + numberToWords(number%1000) 
+	} else if(number < 1000000000){
+		return numberToWords(Math.floor(number/1000000)) + ' juta ' + numberToWords(number%1000000)
+	} else if(number < 1000000000000){
+		return numberToWords(Math.floor(number/1000000000)) + ' milyar ' + numberToWords(number%1000000000)
+	} else if(number < 1000000000000000){
+		return numberToWords(Math.floor(number/1000000000000)) + 'triliun ' + numberToWords(number%1000000000000)
+	}
+  }
+
 
 // Driver code
-console.log(numberToWords(1000000));
-
+//console.log(numberToWords(1000000));
+console.log(numberToWords(999000000000000))
 module.exports = {
   numberToWords: numberToWords
 }
